abc580dbce7b8c9392366e3327af3d3e70135166
nason <nason-ma@users.noreply.github.com>
2021-05-26 07:41:10 +0000

    Apply fixes from StyleCI

--- src/Category.php
+++ src/Category.php
@@ -1,12 +1,18 @@
 <?php
 
+/*
+ * This file is part of the nason/gw_supply_chain.
+ *
+ * (c) nason <mananxun99@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 namespace Nason\GwSupplyChain;
 
-
 class Category extends BaseApi
 {
-
     protected $servicePrefix = 'App.Category.';
 
     public function __construct($wid, $token)
